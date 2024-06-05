347f42022dc5ba2538686a53a72135aed3c9eec8
Jiwon Kim <holiczsia@tamu.edu>
2024-06-05 13:46:21 -0500

    Edited xml files to make Trustmarks appear in the Settings

--- ATAK/app/build/generated/source/buildConfig/civ/debug/com/atakmap/app/BuildConfig.java
+++ ATAK/app/build/generated/source/buildConfig/civ/debug/com/atakmap/app/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.atakmap.app.civ";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "civ";
-  public static final int VERSION_CODE = 1680967075;
-  public static final String VERSION_NAME = "4.6.0.5 (22d11cba)";
+  public static final int VERSION_CODE = 1717564056;
+  public static final String VERSION_NAME = "4.6.0.5 (2db2fdf8)";
   // Fields from build type: debug
   public static final String DEV_BANNER = "";
   // Fields from product flavor: civ
